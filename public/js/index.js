8d46972c061792b93b3cb14e781adc8dbc61e113
Florent Nuttens <fnuttens@gmail.com>
2018-06-09 01:49:34 +0200

    Empty input after sending message

--- public/js/index.js
+++ public/js/index.js
@@ -23,6 +23,6 @@ jQuery('#message-form').on('submit', function(e) {
     from: 'User',
     text: jQuery('[name=message]').val()
   }, function() {
-
+    jQuery('[name=message]').val('');
   });
 });
