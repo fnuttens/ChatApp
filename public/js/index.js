4fd0ca8386c20798afed774cd10376f16ac4f471
Florent Nuttens <fnuttens@gmail.com>
2018-06-09 02:36:04 +0200

    Add geolocation support via geolocation API

--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,5 @@
+/* eslint no-undef: 0 */
+
 var socket = io();
 
 socket.on('connect', function() {
@@ -16,6 +18,17 @@ socket.on('newMessage', function(message) {
   jQuery('#messages').append(li);
 });
 
+socket.on('newLocationMessage', function(message) {
+  var li = jQuery('<li></li>');
+  var a = jQuery('<a target="_blank">My current location</a>');
+
+  li.text(`${message.from}: `);
+  a.attr('href', message.url);
+  li.append(a);
+
+  jQuery('#messages').append(li);
+});
+
 jQuery('#message-form').on('submit', function(e) {
   e.preventDefault();
 
@@ -24,5 +37,22 @@ jQuery('#message-form').on('submit', function(e) {
     text: jQuery('[name=message]').val()
   }, function() {
     jQuery('[name=message]').val('');
+  });
+});
+
+var locationButton = jQuery('#send-location');
+locationButton.on('click', function() {
+  if (!navigator.geolocation) {
+    return alert('Geolocation not supported by your browser.');
+  }
+
+  navigator.geolocation.getCurrentPosition(function(position) {
+    socket.emit('createLocationMessage', {
+      from: 'User',
+      latitude: position.coords.latitude,
+      longitude: position.coords.longitude
+    });
+  }, function() {
+    alert('Unable to fetch location.');
   });
 });
--- server/utils/message.test.js
+++ server/utils/message.test.js
@@ -1,6 +1,6 @@
 const expect = require('expect');
 
-var {generateMessage} = require('./message');
+var {generateMessage, generateLocationMessage} = require('./message');
 
 describe('generateMessage', () => {
   it('should generate correct message object', () => {
@@ -8,9 +8,18 @@ describe('generateMessage', () => {
 
     const message = generateMessage(from, text);
     expect(typeof message.createdAt).toBe('number');
-    expect(message).toMatchObject({
-      from,
-      text
-    });
+    expect(message).toMatchObject({from, text});
+  });
+});
+
+describe('generateLocationMessage', () => {
+  it('should generate correct location object', () => {
+    const from = 'Florent';
+    const lat = 123, lng = 321;
+    const url = `https://www.google.com/maps?q=${lat},${lng}`;
+
+    const locationMessage = generateLocationMessage(from, lat, lng);
+    expect(typeof locationMessage.createdAt).toBe('number');
+    expect(locationMessage).toMatchObject({from, url});
   });
 });
